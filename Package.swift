1d98038c21258cd5cbd3f407a1e37d4951ce53d7
UnityLevelPlay <svc-levelplay-release@unity3d.com>
2026-09-22 09:42:37 +0000

    Release 5.9.0 - SDK 5.3.1

--- Package.swift
+++ Package.swift
@@ -3,12 +3,12 @@ import PackageDescription
 
 let package = Package(
   name: "LevelPlay-Ogury-Adapter-Swift-Package",
-  platforms: [.iOS(.v12)],
+  platforms: [.iOS(.v15)],
   products: [
     .library(name: "OguryAdapter", targets: ["OguryAdapter"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/Ogury/ogury-sdk-spm", exact: "5.3.0"),
+    .package(url: "https://github.com/Ogury/ogury-sdk-spm", exact: "5.3.1"),
     .package(url: "https://github.com/ironsource-mobile/LevelPlay-Swift-Package", "9.0.0"..<"10.0.0"),
   ],
   targets: [
@@ -22,8 +22,8 @@ let package = Package(
     ),
     .binaryTarget(
       name: "OguryAdapterSDK",
-      url: "https://raw.githubusercontent.com/ironsource-mobile/iOS-adapters/master/ogury-adapter/5.8.0/ISOguryAdapter5.8.0.zip",
-      checksum: "dbf010e66c67df647a22d6c57567e02cdd2e199d0b73555c927f046cb6175578"
+      url: "https://raw.githubusercontent.com/ironsource-mobile/iOS-adapters/master/ogury-adapter/5.9.0/ISOguryAdapter5.9.0.zip",
+      checksum: "4d0b8af668b283347607200220a7160abbfd1e212ef0a347286d5450e0f43acf"
     )
   ]
 )
